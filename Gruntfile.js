1ad4f48b4fd7b584aff6035f7a4786eccb849afd
Jill Molloy <hello@jillmolloy.com>
2015-03-02 10:25:02 -0800

    remove imagemin from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,6 @@ module.exports = function(grunt) {
     }
 
   });
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
